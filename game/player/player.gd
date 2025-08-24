85a6d24f0748dfdeabc7526f503a069dee30e4ad
Armand <174164929+NanoMars@users.noreply.github.com>
2025-08-24 20:08:20 +1000

    initial commit for space game

---
+++ game/player/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody3D
+
+@export var move_speed: float = 5.0
+@export var acceleration: float = 10.0
+@export var deceleration: float = 10.0
+
+func _physics_process(delta: float) -> void:
+
+	var input_vector: Vector2 = Vector2(
+		Input.get_action_strength("move_left") - Input.get_action_strength("move_right"),
+		Input.get_action_strength("move_forward") - Input.get_action_strength("move_backward")
+	)
+	if input_vector.length() > 1.0:
+		input_vector = input_vector.normalized()
+
+	var target_velocity: Vector3 = Vector3(input_vector.x, 0.0, input_vector.y) * move_speed
+
+	var rate = acceleration if input_vector != Vector2.ZERO else deceleration
+
+	velocity.x = move_toward(velocity.x, target_velocity.x, rate * delta)
+	velocity.z = move_toward(velocity.z, target_velocity.z, rate * delta)
+
+	move_and_slide()
