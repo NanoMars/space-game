8db71641dd48b2f42a3742ac442126f7237fca79
Armand <174164929+NanoMars@users.noreply.github.com>
2025-09-30 13:36:02 +1000

    leaderboard stuff

--- intermission/win_screen.gd
+++ intermission/win_screen.gd
@@ -5,6 +5,11 @@ var player_name: String = ""
 @export var text_input: LineEdit
 @export var name_select_box: Container
 @export var leaderboard: Leaderboard
+@export var leaderboard_box: Container
+@export var name_container: Container
+@export var score_container: Container
+@export var round_container: Container
+@export var label_theme: Theme
 
 
 func _ready() -> void:
@@ -26,4 +31,32 @@ func _on_name_submit_button_pressed() -> void:
 	leaderboard.fetch_top(25)
 
 func _on_leaderboard_request_completed(data):
+	var leaderboard_tween := create_tween()
+	leaderboard_box.modulate.a = 0.0
+	leaderboard_tween.tween_property(leaderboard_box, "modulate:a", 1.0, 0.5)
+	leaderboard_box.show()
 	print("Leaderboard data received: ", data)
+	for row in data:
+		for column in row:
+			print("  Column: ", column)
+			match column:
+				"name":
+					print("Adding name: ", row[column])
+					var name_label := Label.new()
+					name_label.text = str(row[column])
+					name_label.theme = label_theme
+					name_container.add_child(name_label)
+				"score":
+					print("Adding score: ", row[column])
+					var score_label := Label.new()
+					score_label.text = str(int(row[column]))
+					score_label.theme = label_theme
+					score_container.add_child(score_label)
+				"round":
+					print("Adding round: ", row[column])
+					var round_label := Label.new()
+					round_label.text = str(int(row[column]))
+					round_label.theme = label_theme
+					round_container.add_child(round_label)
+				
+		print("Row: ", row)
