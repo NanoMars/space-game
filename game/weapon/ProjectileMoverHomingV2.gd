48d17af67be9292396daa942966cfb61d423710f
Armand <174164929+NanoMars@users.noreply.github.com>
2025-10-30 01:03:18 +0900

    a

--- game/weapon/ProjectileMoverHomingV2.gd
+++ game/weapon/ProjectileMoverHomingV2.gd
@@ -1,43 +1,33 @@
 extends ProjectileMover
 
-var homing_force: float = 100
-
+var homing_force: float = 10000
 var target_enemy: Node2D
 
 func _ready() -> void:
 	super._ready()
-
-	var enemies: Array = get_tree().get_nodes_in_group("enemies");
-	if not enemies:
-		return
-
-	target_enemy = enemies.pick_random()
-	if not target_enemy:
-		return
-
-	target_enemy.tree_exited.connect(_find_new_enemy)
-
-
-
+	_find_new_enemy()
 
 func _physics_process(delta: float) -> void:
-	if not projectile:
+	if not projectile or not target_enemy:
 		return
 
-	if not target_enemy:
+	if not is_instance_valid(target_enemy):
 		_find_new_enemy()
 		return
 
-	var direction_to_enemy: Vector2 = (projectile.global_position - target_enemy.global_position).normalized()
-	projectile.apply_central_force(direction_to_enemy * delta * homing_force)
-	print("applied force, ", (direction_to_enemy * delta * homing_force))
+	var direction_to_enemy: Vector2 = (target_enemy.global_position - projectile.global_position).normalized()
+	projectile.linear_velocity += direction_to_enemy * homing_force * delta
 
 func _find_new_enemy() -> void:
-	var enemies: Array = get_tree().get_nodes_in_group("enemies");
+	# Disconnect previous signal if target exists
+	if target_enemy and is_instance_valid(target_enemy):
+		target_enemy.tree_exited.disconnect(_find_new_enemy)
+	
+	var enemies: Array = get_tree().get_nodes_in_group("enemies")
 	if enemies.is_empty():
 		target_enemy = null
 		return
 	
 	target_enemy = enemies.pick_random()
 	if target_enemy:
-		target_enemy.tree_exited.connect(_find_new_enemy)
+		target_enemy.tree_exited.connect(_find_new_enemy, CONNECT_ONE_SHOT)
